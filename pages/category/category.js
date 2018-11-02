d2e9c3c53bbbc12e3d482401d6f514678bc4b481
Leon <liang.li@charleskeith.com>
2018-11-02 08:20:48 +0800

    category data

--- pages/category/category.js
+++ pages/category/category.js
@@ -4,22 +4,103 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    tabs: ['all', '福利', '前端', 'Android', 'iOS', '瞎推荐', '拓展资源','休息视频','App'],
+    currentTab: 0,
+    scrollLeft: 0,
+    currentPage: 1,
+    contents: [],
+    contentHeight: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      console.log("fdfsfdsf");
+    var that = this;
+    that.loadingData();
+    wx.getSystemInfo({
+      success: function (res) {
+        var height = res.windowHeight;
+        that.setData({
+          contentHeight: height
+        });
+      }
+    });
   },
 
+  // 点击标题切换当前页时改变样式
+  switchNav: function (e) {
+    var index = e.target.dataset.index;
+    if (this.data.currentTab == index) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: index
+      })
+    }
+  },
+
+  switchTab: function(e) {
+    var that = this;
+    var cur = e.detail.current;
+    this.setData({
+      currentTab: cur
+    });
+    that.checkColor();
+    that.loadingData();
+  },
+
+  checkColor: function() {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
+  },
+  
+  loadingData: function() {
+    wx.showLoading({
+      title: '',
+    })
+    var that = this;
+    var page = that.data.currentPage;
+    var title = that.data.tabs[that.data.currentTab];
+    var url = 'https://gank.io/api/data/' + title + '/10/' + page;
+    wx.request({
+      url: url,
+      success: function (res) {
+        wx.hideLoading();
+        wx.stopPullDownRefresh()
+        wx.hideNavigationBarLoading()
+        if(!res.data.erroer) {
+          var results = res.data.results;
+          if(page == 1) {
+            that.setData({
+              contents: results
+            })
+          } else {
+            var contents = that.data.contents;
+            contents = contents.concat(results);
+            that.setData({
+              contents: contents
+            })            
+          }
+        }
+      }
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
     
   },
+
 
   /**
    * 生命周期函数--监听页面显示
